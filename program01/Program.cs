492de255ab9c03b2260a05f43397b6c898dbf3dd
Ilya Matveev01~ <aorn666@gmail.com>
2022-11-28 15:44:40 +0300

    добавил программу

---
+++ program01/Program.cs
@@ -0,0 +1,39 @@
+﻿// Из имеющегося массива строк сформировать массив строк, длина которых меньше или равна 3
+
+
+
+void PrintArray(string [] collection) 
+{
+    
+
+     Console.WriteLine($"[{string.Join(", ", collection)}]");
+ 
+       Console.WriteLine(); 
+ }
+
+
+void ScreeningArray(string [] collection, string [] rez ) 
+{ 
+   int j = 0; 
+for(int i = 0; i < collection.Length; i++)
+{  
+int c = collection[i].Length;
+   
+    if (c < 4) 
+    {
+        
+        rez[j] = collection[i];
+        j++;
+        
+    }
+}
+} 
+
+
+string [] coll = {"123", "1234", "abc", "abcd", "1a","12ab"}; 
+string [] rez= new string [3];
+Console.Write("Начальный массив: ");
+PrintArray(coll);
+ScreeningArray(coll,rez);
+Console.Write("Конечный массив: ");
+PrintArray(rez);
